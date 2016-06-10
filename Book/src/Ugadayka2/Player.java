d564c298837c6492aba2949e6289937cc59bccfb
AndriiHorbatiuk <andrey.gorbatyuk@gmail.com>
2016-06-10 17:04:13 +0300

    Updated Ugadayka2
    Теперь присутствует ввод данных.

--- Book/src/Ugadayka2/Player.java
+++ Book/src/Ugadayka2/Player.java
@@ -1,11 +1,15 @@
 package Ugadayka2;
 
+import java.util.Scanner;
+
 /**
  * Created by Andrey on 10.06.2016.
  */
 public class Player {
    int number = 0;
+    Scanner scanner = new Scanner(System.in);
     void polzovatelUgadyvaet(){
-        number = (int)(Math.random()*5);
+        System.out.print("Введите число от 0 до 5: ");
+        number = scanner.nextInt();
     }
 }
