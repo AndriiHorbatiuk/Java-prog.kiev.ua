bffa8498acb5f6862f6080a5a4a73263e6facad6
AndriiHorbatiuk <andrey.gorbatyuk@gmail.com>
2016-06-14 15:39:24 +0300

    Lesson 3 added modified HW
    Finished task 5.2

--- lesson3/src/Zadacha_5_2.java
+++ lesson3/src/Zadacha_5_2.java
@@ -6,17 +6,21 @@ import java.util.Arrays;
  * Created by Andrey on 13.06.2016.
  */
 public class Zadacha_5_2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество элементов в массиве: ");
         int n = scanner.nextInt();
         int[] massiv = new int[n];
-        while (n>0){
+        int m = 0;
+        while (m < n) {
             System.out.print("Введите значение: ");
-            massiv[n-1] = scanner.nextInt();
-            --n;
+            massiv[m] = scanner.nextInt();
+            ++m;
         }
         String s = Arrays.toString(massiv);
-        System.out.print(s);
+        System.out.println("Массив: " + s);
+        System.out.print("Какой элемент из массива вывести? ");
+        int num_massiv = scanner.nextInt();
+        System.out.println("Вы выбрали для вывода элемент " + num_massiv +", которому соответствует значение: " + massiv[num_massiv]);
     }
 }
