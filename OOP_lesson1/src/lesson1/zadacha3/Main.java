5e574d225553b4c1aeb73df1153a7f5f461fca38
AndriiHorbatiuk <andrey.gorbatyuk@gmail.com>
2016-07-16 02:37:58 +0300

    Zadacha 3 Done

--- OOP_lesson1/src/lesson1/zadacha3/Main.java
+++ OOP_lesson1/src/lesson1/zadacha3/Main.java
@@ -4,4 +4,12 @@ package lesson1.zadacha3;
  * Created by Andrey on 16.07.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        Turner turner = new Turner();
+        turner.display90();
+        System.out.println();
+        turner.display180();
+        System.out.println();
+        turner.display270();
+    }
 }
--- OOP_lesson1/src/lesson1/zadacha3/Turner.java
+++ OOP_lesson1/src/lesson1/zadacha3/Turner.java
@@ -6,26 +6,44 @@ import java.util.Arrays;
  * Created by Andrey on 16.07.2016.
  */
 public class Turner {
-    static int[][] massiv = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int[][] massiv = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(massiv[0]));
-        System.out.println(Arrays.toString(massiv[1]));
-        System.out.println(Arrays.toString(massiv[2]));
-        System.out.println("");
+    public int[][] turn270Degrees(int[][] x) {
+        int[][] y = new int[x.length][x.length];
+        for (int cols = 0; cols < x.length; cols++) {
+            for (int rows = 0, count = (x[cols].length - 1); rows < x[cols].length; rows++, count--) {
+                y[cols][rows] = x[count][cols];
+            }
+        }
+        return y;
+    }
+
+    public int[][] turn180Degrees(int[][] x) {
+        int[][] result = turn270Degrees(x);
+        result = turn270Degrees(result);
+        return result;
+    }
+
+    public int[][] turn90Degrees(int[][] x) {
+        int[][] result = turn180Degrees(x);
+        result = turn270Degrees(result);
+        return result;
+    }
+
+    public void display90() {
         System.out.println(Arrays.toString(turn90Degrees(massiv)[0]));
         System.out.println(Arrays.toString(turn90Degrees(massiv)[1]));
         System.out.println(Arrays.toString(turn90Degrees(massiv)[2]));
     }
-
-    public static int[][] turn90Degrees(int[][] x) {
-        for (int cols = 0; cols < x.length; cols++) {
-            for (int rows = 0, temp = (x[cols].length - 1); rows < x[cols].length; rows++, temp--) {
-                x[cols][rows] = x[temp][cols];
-//                System.out.println(Arrays.toString(x[cols]));
-//                System.out.println("");
-            }
-        }
-        return x;
+    public void display180() {
+        System.out.println(Arrays.toString(turn180Degrees(massiv)[0]));
+        System.out.println(Arrays.toString(turn180Degrees(massiv)[1]));
+        System.out.println(Arrays.toString(turn180Degrees(massiv)[2]));
+    }
+    public void display270() {
+        System.out.println(Arrays.toString(turn270Degrees(massiv)[0]));
+        System.out.println(Arrays.toString(turn270Degrees(massiv)[1]));
+        System.out.println(Arrays.toString(turn270Degrees(massiv)[2]));
     }
 }
+
